9f0de04d12a317cda3c5f9781d91f9db53630707
cbekoder <cbekoder@gmail.com>
2024-12-17 11:28:44 +0500

    update: adding pagination

--- core/settings.py
+++ core/settings.py
@@ -117,7 +117,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES' : (
         'rest_framework.permissions.IsAuthenticated',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'core.paginations.CustomPagination',
 }
 
 SIMPLE_JWT = {
